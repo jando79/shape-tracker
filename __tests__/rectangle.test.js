8b56821aeaa58d65362e723d74baca1b95522add
David Jandron <jandron1211@gmail.com>
2023-02-01 05:51:18 -0800

    ADD funtion to calculate rectangle, update index.js and index.html to listen and display

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -1,10 +1,37 @@
 import Rectangle from '../src/js/rectangle.js';
 
+import Rectangle from '../src/js/rectangle.js';
+
 describe('Rectangle', () => {
+  let rectangle;
+
+  beforeEach(() => {
+    rectangle = new Rectangle(3,5);
+  });
 
   test('should correctly create a rectangle object using two sides', () => {
-    const rectangle = new Rectangle(3,5);
     expect(rectangle.side1).toEqual(3);
     expect(rectangle.side2).toEqual(5);
   });
+
+  test('should correctly get the area of a rectangle object', () => {
+    expect(rectangle.getArea()).toEqual(15);
+  });
 });
+
+
+//BELOW IS SAME AS ABOVE.ABOVE USES NEW BEFORE(() =>) syntax
+
+//describe('Rectangle', () => {
+
+  //test('should correctly create a rectangle object using two sides', () => {
+    //const rectangle = new Rectangle(3,5);
+    //expect(rectangle.side1).toEqual(3);
+    //expect(rectangle.side2).toEqual(5);
+  //});
+
+  //test('should correctly get the area of a rectangle object', () => {
+    //const rectangle = new Rectangle(3,5);
+    //expect(rectangle.getArea()).toEqual(15);
+  //});
+//});
